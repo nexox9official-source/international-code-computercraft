2b869c1b8a938522b4977288c53cdb31873a8483
nexox9official-source <nexox9official@gmail.com>
2026-09-22 07:38:44 +0200

    chore: bump International Code to v0.3.0

--- international_code/common.lua
+++ international_code/common.lua
@@ -6,7 +6,7 @@ M.CONFIG = M.ROOT .. "/config.tbl"
 M.STATE = M.DATA .. "/state.tbl"
 M.BACKUPS = M.DATA .. "/backups"
 M.PROTOCOL = "uns_icu_v1"
-M.VERSION = "0.2.0"
+M.VERSION = "0.3.0"
 
 local randomReady = false
 
