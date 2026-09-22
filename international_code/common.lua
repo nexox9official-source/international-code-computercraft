deec7033a5f51020d193456f60bdd88f89411e99
nexox9official-source <nexox9official@gmail.com>
2026-09-22 11:39:23 +0200

    chore: bump legal network to v0.15.0

--- international_code/common.lua
+++ international_code/common.lua
@@ -6,7 +6,7 @@ M.CONFIG = M.ROOT .. "/config.tbl"
 M.STATE = M.DATA .. "/state.tbl"
 M.BACKUPS = M.DATA .. "/backups"
 M.PROTOCOL = "uns_icu_v1"
-M.VERSION = "0.14.0"
+M.VERSION = "0.15.0"
 
 local randomReady = false
 
