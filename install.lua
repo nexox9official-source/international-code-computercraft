886c7eed8660b3c06927ee943416abb25f035a09
nexox9official-source <nexox9official@gmail.com>
2026-09-22 14:26:03 +0200

    install: deploy national finance module

--- install.lua
+++ install.lua
@@ -11,6 +11,7 @@ local files={
   "international_code/national_printer.lua",
   "international_code/national_public.lua",
   "international_code/national_services.lua",
+  "international_code/national_finance.lua",
   "international_code/national/corpus_v2.json",
   "international_code/seed/001.lua",
   "international_code/seed/002.lua",
